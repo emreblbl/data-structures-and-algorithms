0b6180a94bd9c271b48810032c9c477056037b09
Emre Bülbül <61878543+emreblbl@users.noreply.github.com>
2024-01-25 07:14:56 +0300

    binary search tree has been added.

---
+++ src/main/java/com/practice/data/structure/algorithms/binarySearchTree/BinarySearchTree.java
@@ -0,0 +1,50 @@
+package com.practice.data.structure.algorithms.binarySearchTree;
+
+public class BinarySearchTree {
+
+    public Node root;
+
+    class Node{
+         int value;
+         Node left;
+         Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public boolean insert(int value){
+        Node node = new Node(value);
+        if(this.root ==null) {
+            root=node;
+            return true;
+        }
+        Node temp = root;
+        while(true){
+            if(node.value == temp.value) return false;
+            if(node.value< temp.value){
+                if(temp.left == null){
+                    temp.left = node;
+                    return true;
+                }
+                temp = temp.left;
+            }else{
+                if(temp.right ==null){
+                    temp.right = node;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+    public boolean contain(int value){
+        Node temp = root;
+        while(temp !=null){
+            if(value<temp.value)  temp = temp.left;
+            else if(value>temp.value) temp = temp.right;
+            else  return true;
+        }
+        return false;
+    }
+}
